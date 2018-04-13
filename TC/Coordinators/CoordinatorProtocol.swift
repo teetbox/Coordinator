f84e2c03beea4b0f84d049b2bf247f6ba317fed6
ttionn <matt.tian@icloud.com>
2018-04-13 16:29:22 +0800

    Add Coordinator protocol

---
+++ TC/Coordinators/CoordinatorProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  CoordinatorProtocol.swift
+//  TC
+//
+//  Created by Matt Tian on 2018/4/13.
+//  Copyright © 2018 TTSY. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {}
